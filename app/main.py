b6648880970df4ce7166b3f45fcac31571bbee0d
Cerasela Nicola Soare <Cerasela-Nicola.Soare@endava.com>
2025-07-24 13:43:52 +0300

    Added prometheus.

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@
 from fastapi import FastAPI
 from app.db import init_db
 from app.routers import math as math_router  # <— noul import
+from prometheus_fastapi_instrumentator import Instrumentator
 
 init_db()
 app = FastAPI(title="Mini-Math API")
 
 app.include_router(math_router.router)  # <— montăm toate rutele
-
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
